cf2103a2a67618cad1e2341a95bfbc253212acf0
Yaroslav Shelest <aht.city@gmail.com>
2021-10-03 13:27:17 +0200

    Added create user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('users')
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<CreateUserDto> {
+    return this.userService.createUser(createUserDto);
+  }
+}
