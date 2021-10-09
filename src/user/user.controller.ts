678fe67a5d4ca65145489b13ed5557d83b468a43
Yaroslav Shelest <y.shelest@platform.live>
2021-10-09 08:44:04 +0200

    Added login endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
 import { UserResponseInterface } from 'src/types/userResponse.interface';
 import { CreateUserDto } from './dto/createUser.dto';
+import { LoginUserDto } from './dto/loginUser.dto';
 import { UserService } from './user.service';
 
 @Controller()
@@ -8,11 +15,20 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
+  @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
-    //console.log('controller', createUserDto);
     const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async loginUser(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.loginUser(loginUserDto);
     return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { sign } from 'jsonwebtoken';
 import { T_SECRET } from 'src/config';
 import { UserResponseInterface } from 'src/types/userResponse.interface';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
+import { LoginUserDto } from './dto/loginUser.dto';
 import { UserEntity } from './user.entity';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -14,10 +16,52 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
+  /**
+   *
+   * @param createUserDto
+   * @returns
+   */
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  /**
+   *
+   * @param loginUserDto
+   * @returns
+   */
+  async loginUser(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      email: loginUserDto.email,
+    });
+    if (!user) {
+      throw new HttpException(
+        'User with email not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const isMatch = await compare(loginUserDto.password, user.password);
+    if (!isMatch) {
+      throw new HttpException('Wrong password', HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    return await this.userRepository.save(user);
   }
 
   generateJwt(user: UserEntity): string {
@@ -32,7 +76,6 @@ export class UserService {
   }
 
   buildUserResponse(user: UserEntity): UserResponseInterface {
-    //({ username, b, ...rest } = user);
     return {
       user: {
         id: user.id,
