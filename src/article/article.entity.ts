cfca5989df540831a2d7d87d386a770ceda0af34
Yaroslav Shelest <y.shelest@platform.live>
2021-10-16 11:44:15 +0200

    Added article entity

---
+++ src/article/article.entity.ts
@@ -0,0 +1,37 @@
+import { timeStamp } from 'console';
+import { BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'articles' })
+export class ArticleEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  'slug': string;
+
+  @Column()
+  'title': string;
+
+  @Column({ default: '' })
+  'description': string;
+
+  @Column({ default: '' })
+  'body': string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  'createdAt': Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  'updatedAt': Date;
+
+  @Column('simple-array')
+  taglist: string[];
+
+  @Column({ default: 0 })
+  favoriteCount: number;
+
+  @BeforeUpdate()
+  updateTimeStamp() {
+    this.updatedAt = new Date();
+  }
+}
