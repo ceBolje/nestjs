a97d4537d3eaea7e57f464bb97acd9d002177b84
Yaroslav Shelest <y.shelest@platform.live>
2021-10-19 06:29:58 +0200

    Added article get by tag

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,20 +1,57 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { User } from 'src/user/decorators/user.decorator';
 import { AuthGuard } from 'src/user/guards/auth.guard';
 import { UserEntity } from 'src/user/user.entity';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/createArticleDto';
+import { ArticleResponseInterface } from './types/articleResponse.type';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
+  /**
+   *
+   * @param slug
+   * @returns ArticleResponseInterface
+   */
+  @Get(':slug')
+  async getArticleBySlug(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.getArticleBySlug(slug);
+    if (!article) {
+      throw new HttpException('Not founf', HttpStatus.NOT_FOUND);
+    }
+    return this.articleService.buildUserResponse(article);
+  }
+
+  /**
+   *
+   * @param currentUser
+   * @param createArticleDto
+   * @returns ArticleResponseInterface
+   */
   @Post('create')
   @UseGuards(AuthGuard)
   async createArticle(
     @User() currentUser: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto,
-  ): Promise<any> {
-    return this.articleService.createArticle(currentUser, createArticleDto);
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
+
+    return this.articleService.buildUserResponse(article);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import slugify from 'slugify';
 import { UserEntity } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
 import { ArticleEntity } from './article.entity';
 import { CreateArticleDto } from './dto/createArticleDto';
+import { ArticleResponseInterface } from './types/articleResponse.type';
 
 @Injectable()
 export class ArticleService {
@@ -12,6 +14,16 @@ export class ArticleService {
     private readonly articleRepository: Repository<ArticleEntity>,
   ) {}
 
+  async getArticleBySlug(slug: string): Promise<ArticleEntity> {
+    return await this.articleRepository.findOne({ slug });
+  }
+
+  /**
+   *
+   * @param currentUser
+   * @param createArticleDto
+   * @returns ArticleEntity
+   */
   async createArticle(
     currentUser: UserEntity,
     createArticleDto: CreateArticleDto,
@@ -21,8 +33,28 @@ export class ArticleService {
 
     article.taglist = article.taglist ?? [];
     article.author = currentUser;
-    article.slug = 'slug';
+    article.slug = this.getSlug(article.title);
 
     return await this.articleRepository.save(article);
+  }
+
+  /**
+   *
+   * @param article
+   * @returns ArticleResponseInterface
+   */
+  buildUserResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  /**
+   *
+   * @returns string
+   */
+  private getSlug(title: string): string {
+    const uniq = ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
+    return `${slugify(title, {
+      lower: true,
+    })}-${uniq}`;
   }
 }
