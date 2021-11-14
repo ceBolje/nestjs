846528f172ea898b3689b0f9104e7c75def2aa39
Yaroslav Shelest <y.shelest@platform.live>
2021-11-14 11:08:15 +0100

    Added follow/unfollow

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,10 +1,13 @@
 import {
   Controller,
+  Delete,
   Get,
-  HttpException,
-  HttpStatus,
   Param,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
+import { User } from 'src/user/decorators/user.decorator';
+import { AuthGuard } from 'src/user/guards/auth.guard';
 import { ProfileService } from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 
@@ -14,9 +17,37 @@ export class ProfileController {
 
   @Get(':username')
   async getProfil(
+    @User('id') currentUserId: number,
     @Param('username') username: string,
   ): Promise<ProfileResponseInterface> {
-    const user = await this.profileService.getProfile(username);
+    const user = await this.profileService.getProfile(currentUserId, username);
+
+    return this.profileService.buildProfileResponse(user);
+  }
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const user = await this.profileService.followProfile(
+      currentUserId,
+      username,
+    );
+
+    return this.profileService.buildProfileResponse(user);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const user = await this.profileService.unfollowProfile(
+      currentUserId,
+      username,
+    );
 
     return this.profileService.buildProfileResponse(user);
   }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
+import { FollowEntity } from './follow.entity';
 import { ProfileType } from './types/profile.types';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 
@@ -10,9 +11,14 @@ export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
-  async getProfile(username: string): Promise<ProfileType> {
+  async getProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
     const user = await this.userRepository.findOne({
       username: username,
     });
@@ -24,7 +30,70 @@ export class ProfileService {
       );
     }
 
-    return { ...user, ...{ following: false } };
+    const follow = await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, ...{ following: Boolean(follow) } };
+  }
+
+  private async findAndCheckUser(
+    currentUserId: number,
+    username: string,
+  ): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      username: username,
+    });
+
+    if (!user) {
+      throw new HttpException(
+        'User with username not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'User and folower are equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return user;
+  }
+
+  async followProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
+    const user = await this.findAndCheckUser(currentUserId, username);
+    const follow = await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    if (!follow) {
+      const newFollow = new FollowEntity();
+      newFollow.followerId = currentUserId;
+      newFollow.followingId = user.id;
+      await this.followRepository.save(newFollow);
+    }
+
+    return { ...user, following: true };
+  }
+
+  async unfollowProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
+    const user = await this.findAndCheckUser(currentUserId, username);
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
